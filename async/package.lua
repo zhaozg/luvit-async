2c1c260694e941bdc1bfdc080765eabc4eb25ada
George Zhao <zhaozg@gmail.com>
2017-09-24 11:06:20 +0800

    bump version and update depends

--- async/package.lua
+++ async/package.lua
@@ -1,10 +1,9 @@
 return {
   name = "virgo-agent-toolkit/async",
-  version = "1.0.3",
+  version = "1.0.3-1",
   description = "async library for luvit",
   author = "Ryan Phillips <ryan.phillips@rackspace.com>",
   dependencies = {
-    "luvit/luvit@2"
   },
   files = {
     "*.lua"
